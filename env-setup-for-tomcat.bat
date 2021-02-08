462bf6d84bd88b9d7751de10619b050d189ec78e
deekshana-veluchamy <78751860+deekshana-veluchamy@users.noreply.github.com>
2021-02-08 23:09:13 +0530

    Add files via upload

---
+++ env-setup-for-tomcat.bat
@@ -0,0 +1,13 @@
+set JAVA_HOME=C:\Program Files (x86)\Java\jdk1.8.0_251
+
+set JRE_HOME=C:\Program Files (x86)\Java\jre1.8.0_251
+
+set PATH="C:\Program Files (x86)\Java\jdk1.8.0_251\bin";%PATH%
+
+set CLASSPATH=.;C:\apache-tomcat-7.0.34\lib\servlet-api.jar;C:\apache-tomcat-7.0.34\lib\jsp-api.jar;C:\apache-tomcat-7.0.34\lib\el-api.jar;C:\apache-tomcat-7.0.34\lib\commons-beanutils-1.8.3.jar;C:\apache-tomcat-7.0.34\lib\mysql-connector-java-5.1.49-bin.jar;
+
+set ANT_HOME=c:\apache-tomcat-7.0.34
+
+set TOMCAT_HOME=C:\apache-tomcat-7.0.34
+
+set CATALINA_HOME=C:\apache-tomcat-7.0.34
